0f84ca50cd4f9fda7f2eac2c31f151559794b5ed
Rahul <Rahulkumar980850@gmail.com>
2023-03-30 04:55:52 +0530

    theme switch functionality added

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
+let body = document.getElementsByTagName('body');
 const CurrentTime = document.getElementById('Curr-time');
+let mode = document.getElementById('theme');
+
+/* for changing color on mode swith */
+let root = document.querySelector(':root');
+let rootStyle = getComputedStyle(root);
+
+let toggleStatus = false;
+
+mode.addEventListener('click', function() {
+
+    /* getting css variable value of red theme */
+    var fg1 = rootStyle.getPropertyValue('--fg1');
+    var fg1Text = rootStyle.getPropertyValue('--fg1Text');
+    var shadow = rootStyle.getPropertyValue('--shadow');
+
+    /* getting css variable value of blue theme */
+    var fg2 = rootStyle.getPropertyValue('--fg2');
+    var fg2Text = rootStyle.getPropertyValue('--fg2Text');
+    var shadow2 = rootStyle.getPropertyValue('--shadow2');
+    
+    if(!toggleStatus) {
+        document.body.style.backgroundImage = "url('./asset/img1.png')";
+
+        root.style.setProperty('--fg1',fg2);
+        root.style.setProperty('--fg1Text',fg2Text);
+        root.style.setProperty('--shadow',shadow2);
+    }else {
+        document.body.style.backgroundImage = "url('./asset/img2.png')";
+    
+        root.style.setProperty('--fg1','#98003f');
+        root.style.setProperty('--fg1Text','#fbf9de');
+        root.style.setProperty('--shadow','rgb(90, 0, 36)');
+    }
+
+    toggleStatus = !toggleStatus;
+    
+})
+
 
 /* every 1sec function will get call */
 setInterval(appendTime, 1000);
